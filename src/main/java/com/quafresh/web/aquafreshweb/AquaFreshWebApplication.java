38a95c4f102dd24283a8d9d90dab73674c4fdb8b
HungPyn <anhbanhoa1102@gmail.com>
2025-03-31 10:32:10 +0700

    first commit

---
+++ src/main/java/com/quafresh/web/aquafreshweb/AquaFreshWebApplication.java
@@ -0,0 +1,13 @@
+package com.quafresh.web.aquafreshweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaFreshWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquaFreshWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quafresh/web/aquafreshweb/AquaFreshWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quafresh.web.aquafreshweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaFreshWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
