826391e3007f94b37caae6a68d7847ca0adc5030
HungPyn <anhbanhoa1102@gmail.com>
2025-04-04 16:25:59 +0700

    update register and login 04/04

--- src/main/java/com/quafresh/web/aquafreshweb/controller/guess/ProductController.java
+++ src/main/java/com/quafresh/web/aquafreshweb/controller/guess/ProductController.java
@@ -6,6 +6,7 @@ import com.quafresh.web.aquafreshweb.service.Impl.PictureGuessServiceImpl;
 import com.quafresh.web.aquafreshweb.service.Impl.ProductDetailImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("product")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class ProductController {
 
     @Autowired
